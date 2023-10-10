03dc3fb586d204fa4181e32d71551f917eb39454
jacksonyoudi <liangchangyoujackson@gmail.com>
2023-10-10 22:54:04 +0800

    init

---
+++ exercises/tests/build.rs
@@ -0,0 +1,20 @@
+//! This is the build script for both tests7 and tests8.
+//!
+//! You should modify this file to make both exercises pass.
+
+
+fn main() {
+    // In tests7, we should set up an environment variable
+    // called `TEST_FOO`. Print in the standard output to let
+    // Cargo do it.
+    // println!("cargo:rustc-env=TEST_FOO=TEST_FOO");
+    // env::set_var("TEST_FOO", "TEST_FOO");
+    // println!("cargo:TEST_FOO=TEST_FOO");
+    let timestamp = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap()
+        .as_secs(); // What's the use of this timestamp here?
+
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp.to_string());
+    println!("cargo:rustc-cfg=feature=\"pass\"");
+}
